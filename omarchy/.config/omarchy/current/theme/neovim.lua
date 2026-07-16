73b8326568427ff3b98eb9bfd95ff62c8dd0b935
shine <kirillisokay@icloud.com>
2026-07-16 12:30:53 +0300

    moved to helix, bash_it, ghostty

--- omarchy/.config/omarchy/current/theme/neovim.lua
+++ omarchy/.config/omarchy/current/theme/neovim.lua
@@ -1,13 +1,12 @@
 return {
-	{
-		"bjarneo/hackerman.nvim",
-		dependencies = { "bjarneo/aether.nvim" }, -- Ensure aether is loaded first
-		priority = 1000,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "hackerman",
-		},
-	},
+  {
+    "omacom-io/lumon.nvim",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "lumon",
+    },
+  },
 }
